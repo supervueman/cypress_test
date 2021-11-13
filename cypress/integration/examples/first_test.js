a8a2fc69ed9de3cb6de071eeea6cd931fb88ee91
supervueman <supervueman@gmail.com>
2021-11-13 15:08:48 +0400

    Negative test

--- cypress/integration/examples/first_test.js
+++ cypress/integration/examples/first_test.js
@@ -1,6 +1,11 @@
 describe('My First Test', () => {
-  it('Verify title of the page', () => {
+  it('Verify title of the page (positive)', () => {
     cy.visit('https://demo.nopcommerce.com/')
     cy.title().should('eq', 'nopCommerce demo store')
+  })
+
+  it('Verify title of the page (negative)', () => {
+    cy.visit('https://demo.nopcommerce.com/')
+    cy.title().should('eq', 'nopCommerce store')
   })
 })
