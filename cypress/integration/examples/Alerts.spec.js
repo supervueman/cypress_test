e0b9044704f583c5263bde3915e67be2adfa4672
supervueman <supervueman@gmail.com>
2021-11-14 20:44:27 +0400

    Add test confirm browser window

--- cypress/integration/examples/Alerts.spec.js
+++ cypress/integration/examples/Alerts.spec.js
@@ -1,11 +1,19 @@
 /// <reference types="Cypress" />
 
 describe('Suite Name', () => {
+  // it('Alerts', () => {
+  //   cy.visit('https://mail.rediff.com/cgi-bin/login.cgi')
+  //   cy.get('input[type=submit]').click()
+  //   cy.on('window:alert', (str) => {
+  //     expect(str).to.equal('Please enter a valid user name')
+  //   })
+  // })
+
   it('Alerts', () => {
-    cy.visit('https://mail.rediff.com/cgi-bin/login.cgi')
-    cy.get('input[type=submit]').click()
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal('Please enter a valid user name')
+    cy.visit('http://testautomationpractice.blogspot.com/')
+    cy.get('#HTML9 > div.widget-content > button').click()
+    cy.on('window:confirm', (str) => {
+      expect(str).to.equal('Press a button!')
     })
   })
 })
